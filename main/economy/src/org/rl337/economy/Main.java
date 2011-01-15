74062cecb831e3cef42ab11dd6c69a9abda6a2f9
unknown <Richard Lee@.(none)>
2011-01-14 20:39:47 -0800

    adding main. totally bogus commit

---
+++ main/economy/src/org/rl337/economy/Main.java
@@ -0,0 +1,17 @@
+package org.rl337.economy;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public static class Resource {
+        
+    }
+    
+    public static enum ResourceType {
+        Textiles
+    }
+}
