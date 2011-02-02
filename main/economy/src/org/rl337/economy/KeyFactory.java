f973cb8110e50c58779de2f325a9a799030860cf
rl337 <rlee@tokyo3.com>
2011-02-02 14:25:11 -0800

    ok. instead of doing custom serialization. Gonna try to use gson for all storage of data.

--- main/economy/src/org/rl337/economy/KeyFactory.java
+++ main/economy/src/org/rl337/economy/KeyFactory.java
@@ -1,12 +1,60 @@
 package org.rl337.economy;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class KeyFactory {
+    private static final Logger smLogger = LoggerFactory.getLogger(KeyFactory.class);
+    
+    @Expose @SerializedName("counters")
     private HashMap<KeyType, KeyCounter> mCounters;
     
     public KeyFactory() {
         mCounters = new HashMap<KeyType, KeyCounter>();
+    }
+    
+    public static KeyFactory loadFile(File file) {
+        Gson gson = new Gson();
+        
+        try {
+        FileReader reader = new FileReader(file);
+            KeyFactory result = gson.fromJson(reader, KeyFactory.class);
+            return result;
+        } catch (IOException e) {
+            return null;
+        }
+        
+    }
+    
+    public static boolean save(File file, KeyFactory factory) {
+        if (file == null) {
+            return false;
+        }
+
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        String sb = gson.toJson(factory);
+        
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(sb.toString());
+            fw.close();
+        } catch (IOException e) {
+            smLogger.error("Could not write to file: " + file.getPath());
+            return false;
+        }
+        
+        return true;
     }
     
     private KeyCounter getKeyCounter(KeyType kt) {
@@ -44,16 +92,25 @@ public class KeyFactory {
     }
     
     private static class KeyCounter {
+        @Expose @SerializedName("value")
         private long mCounter;
+
+        @Expose @SerializedName("increment_by")
         private long mIncrement;
         private volatile Object mLock;
         
-        private KeyCounter(long start, long incBy) {
+        private KeyCounter() {
             mLock = new Object();
+            mIncrement = 1;
+            mCounter = 0;
+        }
+        
+        private KeyCounter(long start, long incBy) {
+            this();
             mCounter = start;
             mIncrement = incBy;
         }
-        
+
         private KeyCounter(long start) {
             this(start, 1);
         }
@@ -70,6 +127,7 @@ public class KeyFactory {
     }
     
     public static enum KeyType implements Comparable<KeyType> {
+        Unknown("XXX"),
         Tick("TIC"),
         Bid("BID"),
         Entity("ENT");
@@ -87,11 +145,33 @@ public class KeyFactory {
         public boolean equals(KeyType k) {
             return mPrefix.equals(k.getPrefix());
         }
+        
+        public static KeyType parseKeyType(String prefix) {
+            if (prefix == null || prefix.length() < 1) {
+                return null;
+            }
+            
+            String uc = prefix.toUpperCase();
+            for(KeyType kt : values()) {
+                if (uc.equals(kt.getPrefix())) {
+                    return kt;
+                }
+            }
+            
+            return null;
+        }
     }
     
     public static class Key implements Comparable<Key> {
+        @Expose @SerializedName("v")
         private long mValue;
+        @Expose @SerializedName("k")
         private KeyType mKeyType;
+        
+        // Create an empty Key.  This exists so that we can gson deserialize this type
+        public Key() {
+            this(KeyType.Unknown, 0);
+        }
         
         public Key(KeyType k, long i) {
             mValue = i;
--- main/economy/tests/org/rl337/economy/KeyFactoryTests.java
+++ main/economy/tests/org/rl337/economy/KeyFactoryTests.java
@@ -1,15 +1,26 @@
 package org.rl337.economy;
 
+import java.io.File;
+import java.util.Random;
+
 import org.rl337.economy.KeyFactory.Key;
 import org.rl337.economy.KeyFactory.KeyType;
 
 import junit.framework.TestCase;
 
 public class KeyFactoryTests extends TestCase {
+    private static final Random smRandom = new Random();
     private KeyFactory mFactory;
+    private File mFile;
     
-    public void setUp() {
+    public void setUp() throws Exception {
         mFactory = new KeyFactory();
+        mFile = File.createTempFile("KeyFactoryTests", ".txt");
+        
+    }
+    
+    public void tearDown() {
+        mFile.delete();
     }
 
     public void testSimpleNewKeys() {
@@ -21,6 +32,26 @@ public class KeyFactoryTests extends TestCase {
         Key l = mFactory.newKey(KeyType.Bid);
         assertEquals("Key type should be Bid", KeyType.Bid, l.getKeyType());
         assertEquals("This key should be 2", 2, l.getValue());
+    }
+    
+    public void testLoadAndSave() {
+        int bidCount = smRandom.nextInt(100);
+        int entityCount = smRandom.nextInt(100);
+        int tickCount = smRandom.nextInt(100);
+        
+        for(int i = 0; i < bidCount; i++) mFactory.newKey(KeyType.Bid);
+        for(int i = 0; i < entityCount; i++) mFactory.newKey(KeyType.Entity);
+        for(int i = 0; i < tickCount; i++) mFactory.newKey(KeyType.Tick);
+        
+        assertTrue("save() should return true.", KeyFactory.save(mFile, mFactory));
+        
+        KeyFactory newFactory = KeyFactory.loadFile(mFile);
+        assertNotNull("load() should have returned returned non-null.", newFactory);
+        
+        assertEquals("bid key should be " + bidCount, bidCount, newFactory.currentKey(KeyType.Bid).getValue());
+        assertEquals("entity key should be " + entityCount, entityCount, newFactory.currentKey(KeyType.Entity).getValue());
+        assertEquals("tick key should be " + tickCount, tickCount, newFactory.currentKey(KeyType.Tick).getValue());
+        assertEquals("unknown key should be 0", 0, newFactory.currentKey(KeyType.Unknown).getValue());
     }
     
 }
