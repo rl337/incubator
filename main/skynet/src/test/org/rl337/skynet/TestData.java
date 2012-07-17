31100cb9d54b5d9308b6ca46651dd9c57f90deac
Richard Lee <rlee@tokyo3.com>
2012-07-17 09:22:07 -0700

    added a method for creating test data for logistic regression

--- main/skynet/src/test/org/rl337/skynet/TestData.java
+++ main/skynet/src/test/org/rl337/skynet/TestData.java
@@ -18,4 +18,20 @@ public class TestData {
         
         return domain.appendColumns(range);
      }
+    
+    public static Matrix testLogisticRegressionMatrix(final double intercept, final double slope, final double minX, final double maxX, final double minY, final double maxY, int count) {
+        final Matrix feature1 = Matrix.random(count, 1, minX, maxX);
+        Matrix feature2 = Matrix.random(count, 1, minY, maxY);
+        
+        Matrix labels = Matrix.matrixOperation(feature2, new MatrixOperation() {
+            public double operation(int row, int col, double val) {
+                double decisionBoundary = slope * feature1.getValue(row, col) + intercept;
+                return val > decisionBoundary ? 1.0 : 0.0;
+            }
+        });
+        
+        Matrix features = feature1.appendColumns(feature2);
+        
+        return features.appendColumns(labels);
+     }
 }
